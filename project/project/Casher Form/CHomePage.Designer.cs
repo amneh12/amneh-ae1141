ee8f259fed8745efd909e3b03a409671b418b9cb
amneh12 <120136271+amneh12@users.noreply.github.com>
2023-05-27 22:03:40 +0300

    Add files via upload

---
+++ project/project/Casher Form/CHomePage.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace project.Casher_Form
+{
+    partial class CHomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblWelcome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(965, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 38);
+            this.label1.TabIndex = 1;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Segoe Print", 16.2F, System.Drawing.FontStyle.Bold);
+            this.lblWelcome.Location = new System.Drawing.Point(12, 7);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(146, 50);
+            this.lblWelcome.TabIndex = 0;
+            this.lblWelcome.Text = "Welcome";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Pink;
+            this.btnAdd.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.Location = new System.Drawing.Point(23, 70);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(130, 60);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Pink;
+            this.btnUpdate.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Bold);
+            this.btnUpdate.Location = new System.Drawing.Point(23, 202);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(130, 60);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Pink;
+            this.btnDelete.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.Location = new System.Drawing.Point(23, 334);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(130, 60);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Pink;
+            this.btnClose.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Bold);
+            this.btnClose.Location = new System.Drawing.Point(23, 466);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(130, 60);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Plum;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Controls.Add(this.btnAdd);
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.btnUpdate);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 723);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Thistle;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(180, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(785, 723);
+            this.panel3.TabIndex = 5;
+            // 
+            // CHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 823);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CHomePage";
+            this.Text = "Casher Page";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.CHomePage_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ project/project/Casher Form/CHomePage.cs
@@ -0,0 +1,124 @@
+﻿using project.Admin_Form.ChangeSale;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project.Casher_Form
+{
+    public partial class CHomePage : Form
+    {
+        string Data = "Data Source=DESKTOP-SV7JT22;Initial Catalog=Project-db;Integrated Security=True";
+
+        private string casher;
+        public CHomePage(string _casher)
+        {
+            InitializeComponent();
+            casher = _casher;
+            LoadData();
+        }
+        private void CHomePage_Load(object sender, EventArgs e)
+        {
+            lblWelcome.Text = "Welcome " + casher;
+        }
+        public void LoadData()
+        {
+            int x = 30;
+            int y = 30;
+            int counter = 0;
+
+            SqlConnection conn = new SqlConnection(Data);
+            conn.Open();
+            string query = "SELECT FoodName, Price, DateSale FROM Sales";
+            SqlCommand sqlCommand = new SqlCommand(query, conn);
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string column1Value = reader.GetString(0);
+                int column2Value = reader.GetInt32(1);
+                DateTime column3Value = reader.GetDateTime(2);
+
+                Panel paneln = new Panel();
+                paneln.Size = new Size(200, 150);
+                paneln.BackColor = Color.LightBlue;
+                panel3.Controls.Add(paneln);
+
+                counter++;
+                paneln.Location = new Point(x, y);
+                x += paneln.Width + 5;
+
+                if (counter == 5)
+                {
+                    y += paneln.Height + 10;
+                    x = 30;
+                    counter = 0;
+                }
+
+                //label Name
+                Label labelname = new Label();
+                labelname.Location = new Point(19, 20);
+                labelname.Text = "FoodName: " + column1Value;
+                paneln.Controls.Add(labelname);
+
+                //labe Price
+                Label labelprice = new Label();
+                labelprice.Location = new Point(19, 45);
+                labelprice.Text = "Price: " + Convert.ToString(column2Value);
+                paneln.Controls.Add(labelprice);
+
+                // labe Date
+                Label labeldate = new Label();
+                DateTimePicker date = new DateTimePicker();
+                labeldate.Location = new Point(19, 70);
+                date.Value = column3Value;
+                labeldate.Text = "Date: " + column3Value;
+                paneln.Controls.Add(labeldate);
+
+                /////////////////////////////////////////////////
+                panel3.Controls.Add(paneln);
+            }
+
+            reader.Close();
+            conn.Close();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddSale addSale = new AddSale();
+            addSale.TopLevel = false;
+            addSale.Dock = DockStyle.Fill;
+            this.panel3.Controls.Clear();
+            this.panel3.Controls.Add(addSale);
+            addSale.Show();
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateSale updateSale = new UpdateSale();
+            updateSale.TopLevel = false;
+            updateSale.Dock = DockStyle.Fill;
+            this.panel3.Controls.Clear();
+            this.panel3.Controls.Add(updateSale);
+            updateSale.Show();
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DeleteSale deleteSale = new DeleteSale();
+            deleteSale.TopLevel = false;
+            deleteSale.Dock = DockStyle.Fill;
+            this.panel3.Controls.Clear();
+            this.panel3.Controls.Add(deleteSale);
+            deleteSale.Show();
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
